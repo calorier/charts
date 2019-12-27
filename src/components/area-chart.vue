819d993f3a5aa0f625baa68b8b7a704554e750f3
Huaiyu Gong <HuaiyuGong123@gmail.com>
2019-12-27 11:49:20 +0800

    feat:structure

---
+++ src/components/area-chart.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="chart">
+    <div id="container" style="min-width:400px;height:400px"></div>设置透明度：
+    <span class="opacity">0.3</span>
+    <div class="range">
+      <span class="min">0.1</span>
+      <input type="range" min="0.1" max="1" step="0.1" value="0.3" />
+      <span class="max">1</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "area-chart",
+  data() {
+    return {};
+  },
+  created() {},
+  watch: {}
+};
+</script>
